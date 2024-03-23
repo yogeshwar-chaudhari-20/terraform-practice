539526603bd74b864d0b445941c8900d7f433c65
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-03-23 12:52:56 +1030

    created first policy using terraform

--- policy-document/main.tf
+++ policy-document/main.tf
@@ -13,5 +13,17 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
+data "aws_iam_policy_document" "bucket_write_policy_statements" {
+  statement {
+    sid = "sid001"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::kzf8wkvqu8/*"]
+    effect = "Allow"
+  }
+}
 
-
+resource "aws_iam_policy" "bucket_write_policy" {
+   name        = "terraform-bucket-write-policy"
+   description = "This policy was generated using terraform"
+   policy = data.aws_iam_policy_document.bucket_write_policy_statements.json
+}
