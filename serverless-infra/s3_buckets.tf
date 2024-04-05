1e65f49feaa08fe89f36acf8178b60b56da82577
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-05 18:09:30 +1030

    creating new s3 bucket for hosting user data

--- serverless-infra/s3_buckets.tf
+++ serverless-infra/s3_buckets.tf
@@ -7,3 +7,22 @@ resource "aws_s3_bucket" "compression_bucket" {
   }
 }
 
+resource "aws_s3_bucket" "user_data_bucket" {
+  bucket = "user_data_bucket"
+
+  tags = {
+    environment = ""
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "user_data_bucket_cors_rule" {
+  bucket = aws_s3_bucket.user_data_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
