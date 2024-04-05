27e766a5bfc22f2ff8d4ee649a4be3e0813e4105
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-06 09:56:23 +1030

    class to get param from param store, getting encrypted param from store at runtime, verified that aws policies are working

--- serverless-infra/hello-world/hello.js
+++ serverless-infra/hello-world/hello.js
@@ -4,9 +4,54 @@
  */
 
 // Lambda function code
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+
+class ParameterStore {
+  constructor(region) {
+    this.ssmClient = new SSMClient({
+      region: region,
+    });
+  }
+
+  async getParameterValue(parameterName) {
+    try {
+      const input = {
+        Name: parameterName,
+        WithDecryption: true,
+      };
+
+      const command = new GetParameterCommand(input);
+      const response = await this.ssmClient.send(command);
+      const parameterValue = response.Parameter.Value;
+
+      return parameterValue;
+    } catch (error) {
+      console.error("Error retrieving parameter value:", error);
+      throw error;
+    }
+  }
+
+  // Singleton pattern to ensure a single instance of ParameterStore is used
+  static getInstance() {
+    if (!ParameterStore.instance) {
+      ParameterStore.instance = new ParameterStore("ap-southeast-2");
+    }
+    return ParameterStore.instance;
+  }
+}
 
 module.exports.handler = async (event) => {
   console.log("Event: ", event);
+  let databasePwd = {};
+  try {
+    const parameterStore = ParameterStore.getInstance();
+    const parameterName = "/development/MONGODB_PWD";
+    const databasePwd = await parameterStore.getParameterValue(parameterName);
+    console.log(databasePwd);
+  } catch (e) {
+    console.error(e);
+  }
+
   let responseMessage = "Hello, World!";
 
   return {
@@ -15,6 +60,7 @@ module.exports.handler = async (event) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      encryptedValue: databasePwd,
       message: responseMessage,
     }),
   };
