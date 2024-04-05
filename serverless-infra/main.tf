cb223ae60fc784fc3120bcf3059728bb98c2c926
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-05 17:46:11 +1030

    creating lambda function

--- serverless-infra/main.tf
+++ serverless-infra/main.tf
@@ -69,4 +69,46 @@ resource "aws_s3_object" "lambda_fn_zip" {
   etag = filemd5(data.archive_file.lambda_fn_zip.output_path)
 }
 
+# Create lambda function
 
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_fn_zip.key
+
+  runtime = "nodejs20.x"
+  handler = "hello.handler"
+
+  source_code_hash = data.archive_file.lambda_fn_zip.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 10
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
