7f1317d2ef506dcc4223d1996ca7e03326904c54
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-05 17:58:49 +1030

    outputting the api_gw invoke url

--- serverless-infra/outputs.tf
+++ serverless-infra/outputs.tf
@@ -2,3 +2,8 @@ output "function_name" {
   description = "Name of the Lambda function."
   value = aws_lambda_function.hello_world.function_name
 }
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+  value = aws_apigatewayv2_stage.lambda.invoke_url
+}
