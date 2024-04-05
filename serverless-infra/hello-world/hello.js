de014a597dcb0d1d370d674e06d249cd24bf53a5
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-05 18:13:54 +1030

    added sample function code

---
+++ serverless-infra/hello-world/hello.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// Lambda function code
+
+module.exports.handler = async (event) => {
+  console.log("Event: ", event);
+  let responseMessage = "Hello, World!";
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+    }),
+  };
+};
