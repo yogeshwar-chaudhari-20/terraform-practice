f142a1defd70c488066ec663547003365d43c245
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-05 17:39:42 +1030

    creating basic s3 bucket to hold the lambda function code

--- serverless-infra/main.tf
+++ serverless-infra/main.tf
@@ -26,3 +26,27 @@ resource "aws_vpc" "default_vpc" {
   }
 }
 
+variable "lamda_bucket_name" {
+  type = string
+  default = "function-code-bucket"
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = var.lamda_bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
+
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
+
