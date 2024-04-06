895db0d4317fe99f4d1bf24ab38abe57d8d27f8a
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-04-06 14:50:59 +1030

    compressor function has access to the s3 buckets with required permissions only

--- import-existing-resources/infra/lambda.tf
+++ import-existing-resources/infra/lambda.tf
@@ -18,9 +18,9 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "lambda_fn_zip" {
   type        = "zip"
-  source_dir = "${path.cwd}/../"
+  source_dir  = "${path.cwd}/../"
   output_path = "${var.env}-user_picture_compressor_fn.zip"
-  excludes = ["infra"]
+  excludes    = ["infra"]
 }
 
 resource "aws_s3_object" "lambda_fn_zip" {
@@ -38,8 +38,8 @@ resource "aws_lambda_function" "user_picture_compressor_lambda" {
   s3_bucket = data.aws_s3_bucket.lambda_bucket.id
   s3_key    = aws_s3_object.lambda_fn_zip.key
 
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "index.handler"
 
   source_code_hash = data.archive_file.lambda_fn_zip.output_base64sha256
 
@@ -51,4 +51,33 @@ resource "aws_lambda_function" "user_picture_compressor_lambda" {
       "DEST_BUCKET" = "${var.env}-user-data-bucket"
     }
   }
+}
+
+# Create a IAM policy JSON to allow PutObject, GetObject operations on buckets
+data "aws_iam_policy_document" "bucket_access_policy_document" {
+  statement {
+    sid       = "sid001"
+    actions   = ["s3:GetObject", "s3:DeleteObject"]
+    resources = ["arn:aws:s3:::${var.env}-compression-bucket/*"]
+    effect    = "Allow"
+  }
+  statement {
+    sid       = "sid002"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${var.env}-user-data-bucket/*"]
+    effect    = "Allow"
+  }
+}
+
+# Create a IAM policy using above JSON statements.
+resource "aws_iam_policy" "compressor_fn_bucket_policy" {
+  name        = "compressor_fn_bucket_policy"
+  description = "Policy to allow lambda to create and get object/s from bucket"
+  policy      = data.aws_iam_policy_document.bucket_access_policy_document.json
+}
+
+# Attaching invoice bucket access policy to Lambda execution role.
+resource "aws_iam_role_policy_attachment" "lambda_bucket_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.compressor_fn_bucket_policy.arn
 }
